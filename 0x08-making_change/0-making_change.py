3b284fd52e92e005b0c53f89d17b04b74996720c
mahi7000 <mahlet7000@gmail.com>
2024-12-01 20:42:25 +0300

    Make change

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 """coins change"""
 
+
 def makeChange(coins, total):
-    """Given a pile of coins of different values, determine the fewest number of coins needed to meet a given amount total"""
+    """Determine the fewest number of coins needed"""
     if (total <= 0):
         return 0
-    
+
     coins = sorted(coins, reverse=True)
     coin_count = 0
 
@@ -15,5 +16,5 @@ def makeChange(coins, total):
 
         if (total == 0):
             return coin_count
-        
+
     return -1
