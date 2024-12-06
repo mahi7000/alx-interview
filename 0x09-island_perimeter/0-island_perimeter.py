e16999210c4bc6d1eebf2b6b1ce6db0f6149b17a
mahi7000 <mahlet7000@gmail.com>
2024-12-06 10:30:21 +0300

    Edge cases

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -11,13 +11,21 @@ def island_perimeter(grid):
             if (element == 0):
                 continue
             else:
-                if (grid[i][j - 1] == 0):
+                if (j != 0 and grid[i][j - 1] == 0):
                     total_perimeter += 1
-                if (grid[i][j + 1] == 0):
+                if (j == 0):
                     total_perimeter += 1
-                if (grid[i - 1][j] == 0):
+                if (j != len(row) - 1 and grid[i][j + 1] == 0):
                     total_perimeter += 1
-                if (grid[i + 1][j] == 0):
+                if (j == len(row) - 1):
+                    total_perimeter += 1
+                if (i != 0 and grid[i - 1][j] == 0):
+                    total_perimeter += 1
+                if (i == 0):
+                    total_perimeter += 1
+                if ( i != len(grid) - 1 and grid[i + 1][j] == 0):
+                    total_perimeter += 1
+                if (i == len(grid) - 1):
                     total_perimeter += 1
 
     return total_perimeter
